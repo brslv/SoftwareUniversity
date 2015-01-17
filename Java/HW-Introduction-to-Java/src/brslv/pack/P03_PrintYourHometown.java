3082a1c5a7e86be61762f3bd95ee1a7d74a1291d
brslv <brslvg@gmail.com>
2015-01-17 12:03:23 +0200

    Updated the solution of the first problem

--- Java/HW-Introduction-to-Java/src/brslv/pack/P03_PrintYourHometown.java
+++ Java/HW-Introduction-to-Java/src/brslv/pack/P03_PrintYourHometown.java
@@ -5,10 +5,24 @@
  */
 package brslv.pack;
 
+import java.util.Scanner;
+
 /**
  *
  * @author brslv
  */
 public class P03_PrintYourHometown {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        // Prompt
+        System.out.println("Please, enter your hometown: ");
+        String hometown = in.nextLine();
+        
+        System.out.println("Your hometown is " + hometown + ". What a surprise, eh? :)");
+        
+    }
     
 }
