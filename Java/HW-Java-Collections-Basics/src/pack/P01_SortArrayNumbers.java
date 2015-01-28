665bd26c46da554960bdf711a71b9f141bfb0d48
brslv <brslvg@gmail.com>
2015-01-28 17:16:49 +0200

    Added the solution to the first problem of the Java Collections homework.

---
+++ Java/HW-Java-Collections-Basics/src/pack/P01_SortArrayNumbers.java
@@ -0,0 +1,31 @@
+package pack;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01_SortArrayNumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int itterations = in.nextInt();
+		in.nextLine();
+		int[] array = new int[itterations];
+		
+		// Filling up the array
+		for (int i = 0; i < itterations; i++) {
+			array[i] = in.nextInt();
+		}
+		
+		// Sorting the array
+		Arrays.sort(array);
+		
+		// Printing the array
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		
+	}
+
+}
